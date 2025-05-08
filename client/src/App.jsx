fb67a2087313de26fb596cbc905de2900c927c45
snehaagrawal17 <151559566+snehaagrawal17@users.noreply.github.com>
2025-05-08 16:35:57 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,33 @@
-import './App.css'
-
-import AppRoutes from './routes/AppRoutes'
-import { BrowserRouter } from 'react-router-dom'
-import { useState } from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import Dashboard from './pages/Dashboard';
+import MedicalRecords from './pages/MedicalRecords';
+import Doctor from './pages/Doctor';
+import Messages from './pages/Messages';
+import Payment from './pages/Payment';
+import Settings from './pages/Settings';
+import Account from './pages/Account';
+import Help from './pages/Help';
+import './App.css';
 
 function App() {
-  
-
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
-  )
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/medical-records" element={<MedicalRecords />} />
+          <Route path="/doctor" element={<Doctor />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/help" element={<Help />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
 }
 
-export default App
+export default App;
